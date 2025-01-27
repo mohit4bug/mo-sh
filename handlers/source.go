9e8fe29d8bdc417d884ff93a04891ecc50f3a575
Mohit Khatri <mohit4bug@gmail.com>
2025-01-27 15:15:04 +0530

    implement handlers for creating and finding all sources

--- handlers/source.go
+++ handlers/source.go
@@ -1,11 +1,79 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/mohit4bug/mo-sh/c"
+	"github.com/mohit4bug/mo-sh/db"
+)
 
 func CreateSource(w http.ResponseWriter, r *http.Request) {
-	// TODO: Implement
+	// TODO: Validate request body
+
+	var body CreateSourceBody
+	c.JSONParseRequestBody(w, r, &body)
+
+	db := db.GetDB()
+	id := c.GenerateULID()
+
+	_, err := db.Exec("INSERT INTO sources (id, name, type) VALUES ($1, $2, $3)", id, body.Name, body.Type)
+	if err != nil {
+		c.JSONResponse(w, http.StatusInternalServerError, c.JSON{
+			"message": "Internal Server Error",
+		})
+		return
+	}
+
+	c.JSONResponse(w, http.StatusCreated, c.JSON{
+		"message": "OK",
+		"data": map[string]interface{}{
+			"id": id,
+		},
+	})
 }
 
 func FindAllSources(w http.ResponseWriter, r *http.Request) {
-	// TODO: Implement
+	db := db.GetDB()
+
+	rows, err := db.Query("SELECT * FROM sources")
+	if err != nil {
+		c.JSONResponse(w, http.StatusInternalServerError, c.JSON{
+			"message": "Internal Server Error",
+		})
+		return
+	}
+	defer rows.Close()
+
+	var sources []Source
+
+	for rows.Next() {
+		var source Source
+
+		if err := rows.Scan(&source.ID, &source.Name, &source.Type); err != nil {
+			c.JSONResponse(w, http.StatusInternalServerError, c.JSON{
+				"message": "Internal Server Error",
+			})
+			return
+		}
+
+		sources = append(sources, source)
+	}
+
+	c.JSONResponse(w, http.StatusOK, c.JSON{
+		"message": "OK",
+		"data": map[string]interface{}{
+			"sources": sources,
+		},
+	})
+}
+
+type CreateSourceBody struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+type Source struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Type string `json:"type"`
 }
