d545b1c37c6a5379cd7620d33411a5bca596dea9
Mohit Khatri <mohit4bug@gmail.com>
2025-01-29 21:58:19 +0530

    add ssh key pair generation handler

--- handlers/ssh-key.go
+++ handlers/ssh-key.go
@@ -1,11 +1,16 @@
 package handlers
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"net/http"
 
 	"github.com/mohit4bug/mo-sh/c"
 	"github.com/mohit4bug/mo-sh/db"
 	"github.com/mohit4bug/mo-sh/models"
+	"golang.org/x/crypto/ssh"
 )
 
 func FindAllSSHKeys(w http.ResponseWriter, r *http.Request) {
@@ -43,4 +48,48 @@ func FindAllSSHKeys(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
-func GenerateKeyPair(w http.ResponseWriter, r *http.Request) {}
+// TODO: Implement support for ED25519 key type.
+func GenerateKeyPair(w http.ResponseWriter, r *http.Request) {
+	// TODO: Validate request body
+
+	var body GenerateKeyPairBody
+	if err := c.JSONParseRequestBody(w, r, &body); err != nil {
+		return
+	}
+
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		c.JSONResponse(w, http.StatusInternalServerError, c.JSON{
+			"message": "Internal Server Error",
+		})
+		return
+	}
+
+	signer, err := ssh.NewSignerFromKey(privateKey)
+	if err != nil {
+		c.JSONResponse(w, http.StatusInternalServerError, c.JSON{
+			"message": "Internal Server Error",
+		})
+		return
+	}
+
+	privatePEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	})
+
+	publicKey := ssh.MarshalAuthorizedKey(signer.PublicKey())
+
+	c.JSONResponse(w, http.StatusOK, c.JSON{
+		"message": "OK",
+		"data": map[string]interface{}{
+			"privateKey": string(privatePEM),
+			"publicKey":  string(publicKey),
+		},
+	})
+
+}
+
+type GenerateKeyPairBody struct {
+	Type string `json:"type"`
+}
